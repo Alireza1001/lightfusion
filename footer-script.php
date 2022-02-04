ad26e9ca231b713206a5f23ed22d392bd9330371
Axoncodes <devops@axoncodes.com>
2022-02-04 08:18:14 -0500

    Commenting the ax_theme_switch_icon and ax_theme_switch_bg

--- footer-script.php
+++ footer-script.php
@@ -52,9 +52,9 @@
         tab_state=1;
         }} if(document.querySelector("header#axon_header .ax-nav-left .ax-menu-btn, section#ax-megaheader")) document.querySelector("header#axon_header .ax-nav-left .ax-menu-btn, section#ax-megaheader").addEventListener("click",ax_nav);
         if(document.getElementById("side_nav"))document.getElementById("side_nav").addEventListener("click",ax_nav);
-        const ax_theme_switch_icon=document.getElementById("ax_theme_switch_icon");
-        var ax_theme_switch_bg;
-         if(document.getElementById("ax_theme_switch")) ax_theme_switch_bg=document.getElementById("ax_theme_switch");
+        // const ax_theme_switch_icon=document.getElementById("ax_theme_switch_icon");
+        // var ax_theme_switch_bg;
+        //  if(document.getElementById("ax_theme_switch")) ax_theme_switch_bg=document.getElementById("ax_theme_switch");
         root.style.setProperty('--ax_service_tab_bg',"#d0d0d0cc");
         root.style.setProperty('--ax_service_tab_active',"#fff");
         root.style.setProperty('--ax_home_offer_p',"#3e3e3e");
@@ -186,11 +186,11 @@
             if(ax_theme_state==1){
                 root.style.setProperty('--lf_country_li_hover',"#5a5a5a");
         root.style.setProperty('--lf_country_li_active',"#7f7f7f");
-        ax_theme_switch_bg.style.backgroundColor="#fdfdfd";
-        ax_theme_switch_icon.style.backgroundColor="#1f1f1f";
-        ax_theme_switch_icon.style.fill="#d8d8d8";
-        ax_theme_switch_icon.innerHTML='<svg id="ax_theme_switch_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79 1.42-1.41zM4 10.5H1v2h3v-2zm9-9.95h-2V3.5h2V.55zm7.45 3.91l-1.41-1.41-1.79 1.79 1.41 1.41 1.79-1.79zm-3.21 13.7l1.79 1.8 1.41-1.41-1.8-1.79-1.4 1.4zM20 10.5v2h3v-2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm-1 16.95h2V19.5h-2v2.95zm-7.45-3.91l1.41 1.41 1.79-1.8-1.41-1.41-1.79 1.8z"/></svg>';
-        ax_theme_switch_icon.style.transform="translate(38px, 4px) rotate(160deg)";
+        // ax_theme_switch_bg.style.backgroundColor="#fdfdfd";
+        // ax_theme_switch_icon.style.backgroundColor="#1f1f1f";
+        // ax_theme_switch_icon.style.fill="#d8d8d8";
+        // ax_theme_switch_icon.innerHTML='<svg id="ax_theme_switch_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0z" fill="none"/><path d="M6.76 4.84l-1.8-1.79-1.41 1.41 1.79 1.79 1.42-1.41zM4 10.5H1v2h3v-2zm9-9.95h-2V3.5h2V.55zm7.45 3.91l-1.41-1.41-1.79 1.79 1.41 1.41 1.79-1.79zm-3.21 13.7l1.79 1.8 1.41-1.41-1.8-1.79-1.4 1.4zM20 10.5v2h3v-2h-3zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6zm-1 16.95h2V19.5h-2v2.95zm-7.45-3.91l1.41 1.41 1.79-1.8-1.41-1.41-1.79 1.8z"/></svg>';
+        // ax_theme_switch_icon.style.transform="translate(38px, 4px) rotate(160deg)";
         root.style.setProperty('--ax_body_bg',"#212121");
         root.style.setProperty('--bg1',"#1b1b1b");
         root.style.setProperty('--bg2',"#212020");
@@ -253,11 +253,11 @@
                 root.style.setProperty('--lf_editor_border',"#f1f1f1");
         root.style.setProperty('--lf_country_li_hover',"#e1e1e1");
         root.style.setProperty('--lf_country_li_active',"#b1b1b1");
-        ax_theme_switch_bg.style.backgroundColor="#4f4f4f";
-        ax_theme_switch_icon.style.backgroundColor="#dcdcdc";
-        ax_theme_switch_icon.style.fill="#171717";
-        ax_theme_switch_icon.innerHTML='<svg id="ax_theme_switch_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12.43 2.3c-2.38-.59-4.68-.27-6.63.64-.35.16-.41.64-.1.86C8.3 5.6 10 8.6 10 12c0 3.4-1.7 6.4-4.3 8.2-.32.22-.26.7.09.86 1.28.6 2.71.94 4.21.94 6.05 0 10.85-5.38 9.87-11.6-.61-3.92-3.59-7.16-7.44-8.1z"/></svg>';
-        ax_theme_switch_icon.style.transform="translate(6px, 4px) rotate(20deg)";
+        // ax_theme_switch_bg.style.backgroundColor="#4f4f4f";
+        // ax_theme_switch_icon.style.backgroundColor="#dcdcdc";
+        // ax_theme_switch_icon.style.fill="#171717";
+        // ax_theme_switch_icon.innerHTML='<svg id="ax_theme_switch_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12.43 2.3c-2.38-.59-4.68-.27-6.63.64-.35.16-.41.64-.1.86C8.3 5.6 10 8.6 10 12c0 3.4-1.7 6.4-4.3 8.2-.32.22-.26.7.09.86 1.28.6 2.71.94 4.21.94 6.05 0 10.85-5.38 9.87-11.6-.61-3.92-3.59-7.16-7.44-8.1z"/></svg>';
+        // ax_theme_switch_icon.style.transform="translate(6px, 4px) rotate(20deg)";
         root.style.setProperty('--ax_body_bg',"#fff");
         root.style.setProperty('--bg1',"#fff");
         root.style.setProperty('--bg2',"#f5f5f5");
