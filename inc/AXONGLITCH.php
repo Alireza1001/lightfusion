acbce87af3ebc9abe36fb1b488040595005ec33c
Axoncodes <devops@axoncodes.com>
2022-02-04 04:11:06 -0500

    Cleanup

--- inc/AXONGLITCH.php
+++ inc/AXONGLITCH.php
@@ -1,83 +1,83 @@
 <?php 
 
 function generateMenuTemplates($menuName) {
-    $content = '
-    <ax-elements 
-    mode="dropdown"
-    headTitlecolor="#FFF4A3"
-    height="70"
-    color="#282A35"
-    colorHover="#fff"
-    activeBackground="#282A35"
-    headBackground="#0000"
-    headBackgroundHover="#04AA6D"
-    structure="dropdownGroup"
-    title="Menu"
-    background="#cbcbcb"
-    subOpening="sub"
-    subTrigger="click">';
-        
-    $menus = wp_get_nav_menus();
-    for ($i=0; $i < count($menus); $i++) {
-      for ($j=$i; $j < count($menus); $j++) {
-        $MenuOrderI = get_field("order_index", wp_get_nav_menu_object($menus[$i]->name))||0;
-        $MenuOrderJ = get_field("order_index", wp_get_nav_menu_object($menus[$j]->name))||0;
-        if($MenuOrderI > $MenuOrderJ) {
-          $temp = $menus[$i];
-          $menus[$i] = $menus[$j];
-          $menus[$j] = $temp;
-        }
+  $content = '
+  <ax-elements 
+  mode="dropdown"
+  headTitlecolor="#FFF4A3"
+  height="70"
+  color="#282A35"
+  colorHover="#fff"
+  activeBackground="#282A35"
+  headBackground="#0000"
+  headBackgroundHover="#04AA6D"
+  structure="dropdownGroup"
+  title="Menu"
+  background="#cbcbcb"
+  subOpening="sub"
+  subTrigger="click">';
+      
+  $menus = wp_get_nav_menus();
+  for ($i=0; $i < count($menus); $i++) {
+    for ($j=$i; $j < count($menus); $j++) {
+      $MenuOrderI = get_field("order_index", wp_get_nav_menu_object($menus[$i]->name))||0;
+      $MenuOrderJ = get_field("order_index", wp_get_nav_menu_object($menus[$j]->name))||0;
+      if($MenuOrderI > $MenuOrderJ) {
+        $temp = $menus[$i];
+        $menus[$i] = $menus[$j];
+        $menus[$j] = $temp;
       }
     }
-    foreach ( $menus as $menu ) :
-        $menuData = wp_get_nav_menu_object($menu->name);
-        if(get_field("location_on_theme", $menuData) == $menuName && (get_field("show_on_empty", $menuData) || count(wp_get_nav_menu_items( $menuData )))) :
-            $structureContent = "";
-            foreach(get_field('structure', $menuData) as $structure) $structureContent .= "$structure ";
-            $content .= "<ax-elements 
-            mode='dropdown'
-            exit='". get_field('exit', $menuData) ."'
-            headTitle='". get_field("headtitle", $menuData) ."'
-            headTitlecolor='". get_field("headtitlecolor", $menuData) ."'
-            height='". get_field("height", $menuData) ."'
-            color='". get_field("color", $menuData) ."'
-            colorHover='". get_field("colorHover", $menuData) ."'
-            activeBackground='". get_field("activeBackground", $menuData) ."'
-            headBackground='". get_field("headBackground", $menuData) ."'
-            headBackgroundHover='". get_field("headBackgroundHover", $menuData) ."'
-            structure='". $structureContent ."'
-            title='". get_field("title", $menuData) ."'
-            background='". get_field("background", $menuData) ."'
-            targetLocator='". strtolower(str_replace(' ', '_', get_field("headtitle", $menuData))) ."_targetLocator'
-            subOpening='". get_field("subopening", $menuData) ."'
-            subTrigger='". get_field("subtrigger", $menuData) ."'
-            options='". json_encode(wordpressAXDropdownContent(wp_get_nav_menu_items($menuData->name))) ."'></ax-elements>";
-        endif; endforeach;
-    $content .= '</ax-elements>';
+  }
+  foreach ( $menus as $menu ) :
+    $menuData = wp_get_nav_menu_object($menu->name);
+    if(get_field("location_on_theme", $menuData) == $menuName && (get_field("show_on_empty", $menuData) || count(wp_get_nav_menu_items( $menuData )))) :
+      $structureContent = "";
+      foreach(get_field('structure', $menuData) as $structure) $structureContent .= "$structure ";
+      $content .= "<ax-elements 
+      mode='dropdown'
+      exit='". get_field('exit', $menuData) ."'
+      headTitle='". get_field("headtitle", $menuData) ."'
+      headTitlecolor='". get_field("headtitlecolor", $menuData) ."'
+      height='". get_field("height", $menuData) ."'
+      color='". get_field("color", $menuData) ."'
+      colorHover='". get_field("colorHover", $menuData) ."'
+      activeBackground='". get_field("activeBackground", $menuData) ."'
+      headBackground='". get_field("headBackground", $menuData) ."'
+      headBackgroundHover='". get_field("headBackgroundHover", $menuData) ."'
+      structure='". $structureContent ."'
+      title='". get_field("title", $menuData) ."'
+      background='". get_field("background", $menuData) ."'
+      targetLocator='". strtolower(str_replace(' ', '_', get_field("headtitle", $menuData))) ."_targetLocator'
+      subOpening='". get_field("subopening", $menuData) ."'
+      subTrigger='". get_field("subtrigger", $menuData) ."'
+      options='". json_encode(wordpressAXDropdownContent(wp_get_nav_menu_items($menuData->name))) ."'></ax-elements>";
+    endif; endforeach;
+  $content .= '</ax-elements>';
 
-    echo $content;
+  echo $content;
 }
 
 // handler menu items to export a json in format of required in AXG library
 function wordpressAXDropdownContent($data) {
-    $level=0;
-    $menuObj=array();
-    $menuObj2=array();
-    for($i=0; $i<count($data); $i++) {
-        $object = new stdClass();
-        $object->title = $data[$i]->title;
-        $object->url = $data[$i]->url;
-        $data[$i]->menu_item_parent==0?$object->level = "undertab":null;
-        if($data[$i]->menu_item_parent == 0) {
-            $object->color = "#FFF4A3";
-            $menuObj[$data[$i]->ID] = $object;
-        } else {
-            // $object->color = "#fff";
-            $menuObj[$data[$i]->menu_item_parent]->content[] = $object;
-        }
+  $level=0;
+  $menuObj=array();
+  $menuObj2=array();
+  for($i=0; $i<count($data); $i++) {
+    $object = new stdClass();
+    $object->title = $data[$i]->title;
+    $object->url = $data[$i]->url;
+    $data[$i]->menu_item_parent==0?$object->level = "undertab":null;
+    if($data[$i]->menu_item_parent == 0) {
+      $object->color = "#FFF4A3";
+      $menuObj[$data[$i]->ID] = $object;
+    } else {
+      // $object->color = "#fff";
+      $menuObj[$data[$i]->menu_item_parent]->content[] = $object;
     }
-    foreach ($menuObj as $value) $menuObj2[] = $value;
-    return $menuObj2;
+  }
+  foreach ($menuObj as $value) $menuObj2[] = $value;
+  return $menuObj2;
 }
 
 function axg_dropdownsbody($menus) {
